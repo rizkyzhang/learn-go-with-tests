d8bb84a9469bc8179cc520d8f9b15848fea910be
rizkyzhang <rizkyzhangdev@gmail.com>
2022-08-02 10:05:18 +0700

    feat: update test case

--- hello-world/hello_test.go
+++ hello-world/hello_test.go
@@ -3,10 +3,32 @@ package main
 import "testing"
 
 func TestHello(t *testing.T) {
-	got := Hello("Golang")
-	want := "Hello, Golang"
+	assertCorrectMessage := func(t testing.TB, got, want string) {
+		t.Helper()
 
-	if (got != want) {
-		t.Errorf("got %q but want %q", got, want)
+		if (got != want) {
+			t.Errorf("got %q but want %q", got, want)
+		}
 	}
+
+	t.Run("Say hello", func(t *testing.T) {
+		got := Hello("Golang", "")
+		want := "Hello, Golang"
+
+		assertCorrectMessage(t, got, want)
+	})
+
+	t.Run("Say 'Hello, World' when empty name string is passed", func(t *testing.T) {
+		got := Hello("", "")
+		want := "Hello, World"
+
+		assertCorrectMessage(t, got, want)
+	})
+
+	t.Run("Say hello in German", func(t *testing.T) {
+		got := Hello("William", "German")
+		want := "Hallo, William"
+
+		assertCorrectMessage(t, got, want)
+	})
 }
--- hello-world/main.go
+++ hello-world/main.go
@@ -2,10 +2,20 @@ package main
 
 import "fmt"
 
-func Hello(name string) string {
-	return fmt.Sprintf("Hello, %s", name)
+
+func Hello(name, language string) string {
+	if (name == "") {
+		name = "World"
+	}
+
+	helloMap := map[string]string{
+		"German": "Hallo",
+		"": "Hello",
+	}
+
+	return fmt.Sprintf("%s, %s", helloMap[language], name)
 }
 
 func main() {
-	fmt.Println(Hello("Golang"))
+	fmt.Println(Hello("Golang", "German"))
 }
